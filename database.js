e6f98dffd390ebb5f652e5055e9691bef6c3501f
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-11-26 10:06:28 +0530

    fix bug

--- database.js
+++ database.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 const MONGODB_URL = process.env.MONGODB_URL;
-mongoose.connect(MONGODB_URL).then(
-  () => console.log(`successfuly connected to DB`),
-  (err) => {
-    // console.log("eee");
-    console.log(err);
-  }
-);
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log(`successfuly connected to DB`))
+  .catch((error) => console.log(error));
 
 module.exports = mongoose;
